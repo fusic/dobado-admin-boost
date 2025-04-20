7e34cf333ceea129351a43414f5c133668394043
ippachi <ippachi1018@gmail.com>
2025-04-20 18:52:22 +0900

    feat: textareaを追加

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import './styles/base.css';
-import './styles/button.css';
-import './styles/input.css';
-import './styles/heading.css';
-import './styles/card.css';
-import './styles/sidebar.css';
-import './styles/typography.css';
+import "./styles/base.css";
+import "./styles/button.css";
+import "./styles/input.css";
+import "./styles/heading.css";
+import "./styles/card.css";
+import "./styles/sidebar.css";
+import "./styles/typography.css";
+import "./styles/caption.css";
